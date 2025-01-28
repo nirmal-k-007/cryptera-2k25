a0808d736eefac2e92aceb866dd78299637df4ee
Your Name <you@example.com>
2025-01-28 15:47:30 +0000

    Updates

---
+++ precache-manifest.6b85bfb6b5db0600e925bd2eeed24f6b.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f5b441cbc0a63f20a323a7075a07d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fb36502e45aa5aebc7",
+    "url": "/static/css/main.b4cd5579.chunk.css"
+  },
+  {
+    "revision": "97833c974df2a53d0274",
+    "url": "/static/js/2.0096e880.chunk.js"
+  },
+  {
+    "revision": "10fb36502e45aa5aebc7",
+    "url": "/static/js/main.25cd9cec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6334d65cea7c29bf91152ee7031b2935",
+    "url": "/static/media/0xdebug.6334d65c.jpg"
+  },
+  {
+    "revision": "0a965112c0536fcf2fad7e6e055e3dca",
+    "url": "/static/media/Ajai.0a965112.jpeg"
+  },
+  {
+    "revision": "cf6fed8b77cd6bc6d8862545c1e25780",
+    "url": "/static/media/AkshayaRB.cf6fed8b.jpeg"
+  },
+  {
+    "revision": "4d98f0b4b7618562d9744b347206e152",
+    "url": "/static/media/Aniruthan.4d98f0b4.jpeg"
+  },
+  {
+    "revision": "ae3c8c21f6e454357b9a937cf223e036",
+    "url": "/static/media/Anuvarshini.ae3c8c21.jpeg"
+  },
+  {
+    "revision": "2ec089635613b52be3e6889b0c0bdfb4",
+    "url": "/static/media/Arshad.2ec08963.jpeg"
+  },
+  {
+    "revision": "2ccb768c8ddcfd6e59a22f417a6e2b62",
+    "url": "/static/media/CIT-Coimbatore.2ccb768c.jpg"
+  },
+  {
+    "revision": "83954b5409c178f1a469cffb28ae7e85",
+    "url": "/static/media/Faizan.83954b54.jpeg"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "ba9931f573fb536b61d7692cd7a5a963",
+    "url": "/static/media/Gladwin.ba9931f5.jpeg"
+  },
+  {
+    "revision": "37078811e58ab75bb88c5a9a2386c395",
+    "url": "/static/media/Gokulajay.37078811.jpeg"
+  },
+  {
+    "revision": "ba4646b5a50d5e82b8853bc45b5ec07c",
+    "url": "/static/media/Gowtham.ba4646b5.jpeg"
+  },
+  {
+    "revision": "9dd4814ae6b60f79d8e6e4901ede7d24",
+    "url": "/static/media/Hanush.9dd4814a.jpeg"
+  },
+  {
+    "revision": "2795441c3f4fa583ccc7f3b5ac3a317a",
+    "url": "/static/media/HarshalRam.2795441c.jpeg"
+  },
+  {
+    "revision": "c828f925460bb6f0825b67bb0faa3cd6",
+    "url": "/static/media/Janani.c828f925.jpeg"
+  },
+  {
+    "revision": "d62ff3111aabf5ad79519abe9f92d5e0",
+    "url": "/static/media/Jawahar.d62ff311.jpeg"
+  },
+  {
+    "revision": "0b5b3af36bb85e055f4f73b164b6c265",
+    "url": "/static/media/Jumbled Code.0b5b3af3.jpg"
+  },
+  {
+    "revision": "a08a9ea2f0dee8e35ab19d34e75075e2",
+    "url": "/static/media/Karupaiya.a08a9ea2.jpeg"
+  },
+  {
+    "revision": "a5c5e5e9e06b240660d118d65c60ccf3",
+    "url": "/static/media/Krithika.a5c5e5e9.jpeg"
+  },
+  {
+    "revision": "4853730d897d6789a42d17e95fc842cd",
+    "url": "/static/media/Mukilan.4853730d.jpeg"
+  },
+  {
+    "revision": "9bc800771270d5281be11194eef443ea",
+    "url": "/static/media/Muthu.9bc80077.jpeg"
+  },
+  {
+    "revision": "6e6ba1c1655d8bbce6e21728aa53f62e",
+    "url": "/static/media/Narmada.6e6ba1c1.jpeg"
+  },
+  {
+    "revision": "9f42dfddb7bc38a485cdfa6fb1c5879b",
+    "url": "/static/media/NaveenKumar.9f42dfdd.jpeg"
+  },
+  {
+    "revision": "d8576197889b82d5b9342b500afb0a28",
+    "url": "/static/media/Niranjan.d8576197.jpeg"
+  },
+  {
+    "revision": "12e139264e6033fe86656b5780db6963",
+    "url": "/static/media/Prakash.12e13926.jpeg"
+  },
+  {
+    "revision": "6235b01b9895eb63c984cea65dcc1b6f",
+    "url": "/static/media/Priyadharshini.6235b01b.jpeg"
+  },
+  {
+    "revision": "94be3fc5eafd4664ebb68137724e7f1d",
+    "url": "/static/media/Priyanth.94be3fc5.jpeg"
+  },
+  {
+    "revision": "df97790c4c57e15493bc3e05b49c60b0",
+    "url": "/static/media/Rajaponpriya.df97790c.jpeg"
+  },
+  {
+    "revision": "0e4c890195eca3a4fa0d20763333f4a1",
+    "url": "/static/media/UI design Contest.0e4c8901.jpg"
+  },
+  {
+    "revision": "b94f26671f4b93a886fcaa55e9896915",
+    "url": "/static/media/banner.b94f2667.jpg"
+  },
+  {
+    "revision": "dbd9564f66227323510dd0d2e3a8b6d9",
+    "url": "/static/media/blindCode.dbd9564f.jpg"
+  },
+  {
+    "revision": "9763ae16f5261f5beff5d5cb6e9aa734",
+    "url": "/static/media/blockchain.9763ae16.jpg"
+  },
+  {
+    "revision": "047e3c4a64423cd43cb3a69b09f300b5",
+    "url": "/static/media/carrom.047e3c4a.jpg"
+  },
+  {
+    "revision": "0f16b1571bfe5b224aad03ee2a35f994",
+    "url": "/static/media/chess.0f16b157.jpg"
+  },
+  {
+    "revision": "d39268cfe6ac3c6aeca61ad680f361cb",
+    "url": "/static/media/crypterabg.d39268cf.png"
+  },
+  {
+    "revision": "3ae018ad311032aa60f1561ef6558de9",
+    "url": "/static/media/flagship.3ae018ad.png"
+  },
+  {
+    "revision": "38b1d90c8fcbb7047ecbca212404e50e",
+    "url": "/static/media/jumbledCode.38b1d90c.jpg"
+  },
+  {
+    "revision": "17b75ea94faf2b04fe5f4667d42d29ac",
+    "url": "/static/media/layer11.17b75ea9.webp"
+  },
+  {
+    "revision": "bbcbec3863351124cf11ac4ca4c4b477",
+    "url": "/static/media/layer5.bbcbec38.webp"
+  },
+  {
+    "revision": "5aaf0e153110d380f78187ddaaaa489b",
+    "url": "/static/media/layer6.5aaf0e15.webp"
+  },
+  {
+    "revision": "ca48d51f4e4a1c31f23237f9385816e5",
+    "url": "/static/media/layer7.ca48d51f.png"
+  },
+  {
+    "revision": "d10b113832f9b3d08e19a696accf4e9f",
+    "url": "/static/media/layer8.d10b1138.webp"
+  },
+  {
+    "revision": "f464b7ee6c061dc4bebc842104be24af",
+    "url": "/static/media/layer9.f464b7ee.webp"
+  },
+  {
+    "revision": "213e51e2f5368163f6307e24b7326cbb",
+    "url": "/static/media/loading.213e51e2.gif"
+  },
+  {
+    "revision": "d467913df6e9f864a42e5613bd89fb46",
+    "url": "/static/media/logo_final.d467913d.png"
+  },
+  {
+    "revision": "e35e5a5c888f6d2d4d81bc5d5115985c",
+    "url": "/static/media/male-avatar.e35e5a5c.jpeg"
+  },
+  {
+    "revision": "a8efc1f3bf633254d22ae69ead20f0fe",
+    "url": "/static/media/non-technical.a8efc1f3.png"
+  },
+  {
+    "revision": "978ef9333c8530ddddbf3bdb4d6b7e24",
+    "url": "/static/media/paper presentation.978ef933.jpg"
+  },
+  {
+    "revision": "d41a8b1c2cbc71157027bc5b35eb3e89",
+    "url": "/static/media/pcrypterabg.d41a8b1c.png"
+  },
+  {
+    "revision": "f0ca04eb67190996a2b20c634d5ba956",
+    "url": "/static/media/pflagship.f0ca04eb.png"
+  },
+  {
+    "revision": "b1c0f550370d509544b03bd0205efb46",
+    "url": "/static/media/pnon-technical.b1c0f550.png"
+  },
+  {
+    "revision": "d36baf53e1a3d8979decd9e5dcd6b4d1",
+    "url": "/static/media/principal.d36baf53.jpg"
+  },
+  {
+    "revision": "2428955e9b5060b2611a8091e2c7f2cb",
+    "url": "/static/media/prompteng.2428955e.jpeg"
+  },
+  {
+    "revision": "4d7a7eec957decbf2ec594cc0d59e3e5",
+    "url": "/static/media/ptechnical.4d7a7eec.png"
+  },
+  {
+    "revision": "406d60bf689f8a78e3d2f655a930a995",
+    "url": "/static/media/space5.406d60bf.jpg"
+  },
+  {
+    "revision": "fbb359c2b5d042b8e9dc9dafb26b69ef",
+    "url": "/static/media/startup.fbb359c2.jpg"
+  },
+  {
+    "revision": "fc81a56af3b4066e8786724f6b708568",
+    "url": "/static/media/technical.fc81a56a.png"
+  },
+  {
+    "revision": "56b6224e935afdee4ae874ffbf5ef316",
+    "url": "/static/media/workshop1.56b6224e.jpg"
+  }
+]);
